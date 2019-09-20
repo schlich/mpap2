d37ef79d9b14b0d2c8c6d663831cb8d281502a41
Tyler Schlichenmeyer <tyler.schlichenmeyer@gmail.com>
2019-09-20 18:48:44 -0500

    race breakdown pie chart

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import os
-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import pandas as pd
+import plotly.graph_objs as go
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -10,20 +11,28 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 server = app.server
 
+data = pd.read_csv('cleaned_police_data.csv')
+
+race_counts = data['race of complainant'].value_counts()
+
 app.layout = html.Div([
-    html.H2('Hello World'),
-    dcc.Dropdown(
-        id='dropdown',
-        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
-        value='LA'
+    html.H2('St Louis Police Complaints'),
+    dcc.Graph(
+		figure=go.Figure(
+			data=go.Pie(
+				labels=race_counts.index.tolist(),
+				values=race_counts.values.tolist(),
+				textinfo='label+value'
+			)
+		)
     ),
     html.Div(id='display-value')
 ])
 
-@app.callback(dash.dependencies.Output('display-value', 'children'),
-              [dash.dependencies.Input('dropdown', 'value')])
-def display_value(value):
-    return 'You have selected "{}"'.format(value)
+# @app.callback(dash.dependencies.Output('display-value', 'children'),
+#               [dash.dependencies.Input('dropdown', 'value')])
+# def display_value(value):
+#     return 'You have selected "{}"'.format(value)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
